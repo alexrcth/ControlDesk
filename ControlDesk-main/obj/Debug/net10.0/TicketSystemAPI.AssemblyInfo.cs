00ce5f6e70a5fa52f94beacab768cb057efa7c99
RODRIGUEZ CHAVEZ <alexander.rodriguez-chavez.ext@siemens.com>
2026-05-04 16:57:01 -0600

    Actualizando entidades del dominio y DTOs

--- ControlDesk-main/obj/Debug/net10.0/TicketSystemAPI.AssemblyInfo.cs
+++ ControlDesk-main/obj/Debug/net10.0/TicketSystemAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketSystemAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5118bc909fa429a0d00f90d4f7e7d21604996fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketSystemAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketSystemAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
